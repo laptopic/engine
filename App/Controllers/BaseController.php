53dc9cca090756863c34fc8dd02f1a0090fed39d
laptopic <furik73@mail.ru>
2020-12-05 11:25:03 +0300

    мелкие правки

--- App/Controllers/BaseController.php
+++ App/Controllers/BaseController.php
@@ -26,29 +26,4 @@ class BaseController
             require 'App/Views/' . $this->layout . '.php';
         }
     }
-
-    public function template($view) {
-
-        foreach($this->data as $k=>$v) $$k=$v;
-
-        $content_main = $this->load_template($view);
-
-        if(file_exists("views/{$this->layout}.php"))
-            include "views/{$this->layout}.php";
-    }
-
-
-    public function load_template($view) {
-
-        foreach($this->data as $k=>$v) $$k=$v;
-
-        ob_start();
-        if(file_exists("views/$view.php"))
-            include "views/$view.php";
-
-        $template = ob_get_contents();
-        ob_end_clean();
-        return $template;
-    }
-
 }
